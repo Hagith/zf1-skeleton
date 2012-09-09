6e09984b1456844e3e2896d008a5b85228b5fd5f
Rafal Galka <rafal.galka@modernweb.pl>
2012-09-09 21:53:11 +0200

    docblock

--- application/models/Website/Exception.php
+++ application/models/Website/Exception.php
@@ -3,10 +3,21 @@
 /**
  * ModernWeb
  *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.modernweb.pl/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to kontakt@modernweb.pl so we can send you a copy immediately.
+ *
  * @category    Modern
  * @package     Website
  * @author      Rafał Gałka <rafal@modernweb.pl>
  * @copyright   Copyright (c) 2007-2012 ModernWeb (http://www.modernweb.pl)
+ * @license     http://www.modernweb.pl/license/new-bsd     New BSD License
  */
 
 /**
